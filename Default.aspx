14e4812c8fbabc066cd03a4b805a574c981565e0
unknown <suleymanbayazitt@gmail.com>
2020-01-06 02:14:34 +0300

    aksesuar

---
+++ Default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 543px;
+        }
+        .auto-style3 {
+            width: 575px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">
+                    <asp:Panel ID="Panel1" runat="server" GroupingText="Araba Satış Fiyatı" Height="125px">
+                        &nbsp;
+                        <br />
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                        &nbsp; TL<br />
+                        <br />
+                    </asp:Panel>
+                </td>
+                <td class="auto-style3">
+                    <asp:Panel ID="Panel2" runat="server" GroupingText="Aksesuar" Height="129px">
+                        <asp:CheckBox ID="CheckBox1" runat="server" Text="Hız Sabitleyici" />
+                        <br />
+                        <br />
+                        <asp:CheckBox ID="CheckBox2" runat="server" OnCheckedChanged="CheckBox2_CheckedChanged" Text="Yol Bilgisayarı" />
+                        <br />
+                        <br />
+                        <asp:CheckBox ID="CheckBox3" runat="server" OnCheckedChanged="CheckBox3_CheckedChanged" Text="Otomatik Vites" />
+                    </asp:Panel>
+                </td>
+            </tr>
+        </table>
+    <div>
+    
+        Hız Sabitleyici 2000 TL , Yol Bilgisayarı 3000 TL , Otomatik Vites 5000 TL</div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TextBox1.AutoPostBack = true;
+        CheckBox1.AutoPostBack = true;
+        CheckBox2.AutoPostBack = true;
+        CheckBox3.AutoPostBack = true;
+        TextBox1.Text = "100000";
+        
+    }
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+        if (CheckBox1.Checked)
+            TextBox1.Text = (int.Parse(TextBox1.Text) + 2000).ToString();
+        else
+            TextBox1.Text = (int.Parse(TextBox1.Text) + 2000).ToString();
+    }
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+        CheckBox1.Checked = false;
+        CheckBox2.Checked = false;
+        CheckBox3.Checked = false;
+    }
+    protected void CheckBox2_CheckedChanged(object sender, EventArgs e)
+    {
+        if (CheckBox2.Checked)
+            TextBox1.Text = (int.Parse(TextBox1.Text) + 2000).ToString();
+        else
+            TextBox1.Text = (int.Parse(TextBox1.Text) + 2000).ToString();
+    }
+    protected void CheckBox3_CheckedChanged(object sender, EventArgs e)
+    {
+        if (CheckBox3.Checked)
+            TextBox1.Text = (int.Parse(TextBox1.Text) + 2000).ToString();
+        else
+            TextBox1.Text = (int.Parse(TextBox1.Text) + 2000).ToString();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (Convert.ToInt32(TextBox1.Text) > 100000)
+            Label1.Text = "%5 indirimli fiyatı" + (int.Parse(TextBox1.Text) * 0.95).ToString();
+        if (Convert.ToInt32(TextBox1.Text) < 100000)
+            Label1.Text = "%3 indirimli fiyatı" + (int.Parse(TextBox1.Text) * 0.97).ToString();
+    }
+}
